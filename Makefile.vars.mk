9c47ce73235986c8688715ee7353e5ba3a2e9e96
Simon Beck <simon.beck@vshn.ch>
2023-02-06 10:56:05 +0100

    Take over kind settings from provider-exoscale

--- Makefile.vars.mk
+++ Makefile.vars.mk
@@ -13,7 +13,7 @@ DOCKER_CMD ?= docker
 ## KIND:setup
 
 # https://hub.docker.com/r/kindest/node/tags
-KIND_NODE_VERSION ?= v1.23.0
+KIND_NODE_VERSION ?= v1.24.4
 KIND_IMAGE ?= docker.io/kindest/node:$(KIND_NODE_VERSION)
 KIND_CMD ?= go run sigs.k8s.io/kind
 KIND_KUBECONFIG ?= $(kind_dir)/kind-kubeconfig-$(KIND_NODE_VERSION)
--- kind/kind.mk
+++ kind/kind.mk
@@ -1,35 +1,47 @@
-kind_dir ?= .kind
-ingress_sentinel = $(kind_dir)/ingress-sentinel
+go_bin ?= $(PWD)/.work/bin
+$(go_bin):
+	@mkdir -p $@
 
+kind_dir ?= $(PWD)/.kind
+kind_bin = $(go_bin)/kind
+
+# Prepare kind binary
+$(kind_bin): export GOOS = $(shell go env GOOS)
+$(kind_bin): export GOARCH = $(shell go env GOARCH)
+$(kind_bin): export GOBIN = $(go_bin)
+$(kind_bin): | $(go_bin)
+	go install sigs.k8s.io/kind@latest
+
+
+.PHONY: kind
 kind: export KUBECONFIG = $(KIND_KUBECONFIG)
-kind: kind-setup-ingress ## All-in-one kind target
+kind: kind-setup-ingress kind-load-image ## All-in-one kind target
 
+.PHONY: kind-setup
 kind-setup: export KUBECONFIG = $(KIND_KUBECONFIG)
 kind-setup: $(KIND_KUBECONFIG) ## Creates the kind cluster
 
+.PHONY: kind-setup-ingress
 kind-setup-ingress: export KUBECONFIG = $(KIND_KUBECONFIG)
-kind-setup-ingress: $(ingress_sentinel) ### Install NGINX as ingress controller onto kind cluster (localhost:8081 / localhost:8443)
-
-$(ingress_sentinel): export KUBECONFIG = $(KIND_KUBECONFIG)
-$(ingress_sentinel): $(KIND_KUBECONFIG)
+kind-setup-ingress: kind-setup ## Install NGINX as ingress controller onto kind cluster (localhost:8081)
 	kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-	kubectl wait -n ingress-nginx --for condition=Complete jobs/ingress-nginx-admission-patch --timeout 180s
-	kubectl wait -n ingress-nginx --for condition=ContainersReady --timeout 180s $$(kubectl -n ingress-nginx get pods -o name --no-headers | grep controller)
-	@touch $@
 
-# .PHONY: kind-load-image
-# kind-load-image: kind-setup build-docker ### Load the container image onto kind cluster
-# 	@$(KIND) load docker-image --name $(KIND_CLUSTER) $(CONTAINER_IMG)
+.PHONY: kind-load-image
+# We fix the arch to linux/amd64 since kind runs in amd64 even on Mac/arm.
+kind-load-image: export GOOS = linux
+kind-load-image: export GOARCH = amd64
+kind-load-image: kind-setup build-docker ## Load the container image onto kind cluster
+	@$(kind_bin) load docker-image --name $(KIND_CLUSTER) $(CONTAINER_IMG)
 
 .PHONY: kind-clean
 kind-clean: export KUBECONFIG = $(KIND_KUBECONFIG)
 kind-clean: ## Removes the kind Cluster
-	@$(KIND_CMD) delete cluster --name $(KIND_CLUSTER) || true
-	@rm -rf $(kind_dir)/*
+	@$(kind_bin) delete cluster --name $(KIND_CLUSTER) || true
+	rm -rf $(kind_dir) $(kind_bin)
 
 $(KIND_KUBECONFIG): export KUBECONFIG = $(KIND_KUBECONFIG)
-$(KIND_KUBECONFIG):
-	$(KIND_CMD) create cluster \
+$(KIND_KUBECONFIG): $(kind_bin)
+	$(kind_bin) create cluster \
 		--name $(KIND_CLUSTER) \
 		--image $(KIND_IMAGE) \
 		--config kind/config.yaml
